499aaf48ff59dc14aaa428074d0f9aa48e53a599
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-09-18 12:15:10 +0200

    Implemented lab 1

--- frontend-web/customer-app/src/app/app.component.ts
+++ frontend-web/customer-app/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-hallo',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'customer-app';
+  title = 'Web expert!';
+  name = 'Su-zen Geurts';
 }
