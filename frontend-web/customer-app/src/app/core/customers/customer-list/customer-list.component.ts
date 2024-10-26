18582e08694fe3a67097c5c0fc9a35e482e8584f
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-10-26 23:10:45 +0200

    [FRONTEND] Lab 5: Forms
    Added filters for name, city and VAT (Template Driven)

--- frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.component.ts
+++ frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {CustomerItemComponent} from "../customer-item/customer-item.component";
 import {FilterComponent} from "../filter/filter.component";
 import {Customer} from "../../../shared/models/customer.model";
+import {Filter} from "../../../shared/models/filter.model";
 
 @Component({
   selector: 'app-customer-list',
@@ -12,6 +13,7 @@ import {Customer} from "../../../shared/models/customer.model";
 })
 export class CustomerListComponent implements OnInit {
   customers!: Customer[];
+  filteredData!: Customer[];
 
   ngOnInit(): void {
     this.customers = [
@@ -21,6 +23,21 @@ export class CustomerListComponent implements OnInit {
     ];
 
     this.customers[1].isLoyal = true;
+    this.filteredData = this.customers;
+  }
+
+  handleFilter(filter: Filter){
+    this.filteredData = this.customers.filter(customer => this.isCustomerMatchingFilter(customer, filter));
+  }
+
+  private isCustomerMatchingFilter(customer: Customer, filter: Filter): boolean {
+    const matchesName = customer.name.toLowerCase().includes(filter.name.toLowerCase());
+    const matchesCity = customer.city.toLowerCase().includes(filter.city.toLowerCase());
+    const matchesVat = filter.vat ? customer.vat === filter.vat : true;
+
+    return matchesName && matchesCity && matchesVat;
   }
 }
+
+
 
