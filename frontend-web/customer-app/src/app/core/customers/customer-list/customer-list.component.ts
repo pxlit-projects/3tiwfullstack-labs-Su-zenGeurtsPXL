49d9a03b41283e8928d885abb9368fc469dd6d0e
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-10-28 13:14:13 +0100

    [FRONTEND] Lab 6: Services and DI Added Customer Service and refactored relevant code

--- frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.component.ts
+++ frontend-web/customer-app/src/app/core/customers/customer-list/customer-list.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {CustomerItemComponent} from "../customer-item/customer-item.component";
 import {FilterComponent} from "../filter/filter.component";
 import {Customer} from "../../../shared/models/customer.model";
 import {Filter} from "../../../shared/models/filter.model";
 import {AddCustomerComponent} from "../add-customer/add-customer.component";
+import {CustomerService} from "../../../shared/services/customer.service";
 
 @Component({
   selector: 'app-customer-list',
@@ -16,32 +17,21 @@ export class CustomerListComponent implements OnInit {
   customers!: Customer[];
   filteredData!: Customer[];
 
-  ngOnInit(): void {
-    this.customers = [
-      new Customer('Dries Swinnen', 'dries@pxl.be', 'Pelt', 'mystreet', 'Belgium', 21),
-      new Customer('John Doe', 'john@doe.be', 'New York', '5th Avenue', 'USA', 6),
-      new Customer('Jane Doe', 'jane@doe.be', 'Los Angeles', 'Sunset Boulevard', 'USA', 6),
-    ];
+  customerService: CustomerService = inject(CustomerService);
 
+  ngOnInit(): void {
+    this.customers = this.customerService.getCustomers();
     this.customers[1].isLoyal = true;
     this.filteredData = this.customers;
   }
 
   handleFilter(filter: Filter){
-    this.filteredData = this.customers.filter(customer => this.isCustomerMatchingFilter(customer, filter));
-  }
-
-  private isCustomerMatchingFilter(customer: Customer, filter: Filter): boolean {
-    const matchesName = customer.name.toLowerCase().includes(filter.name.toLowerCase());
-    const matchesCity = customer.city.toLowerCase().includes(filter.city.toLowerCase());
-    const matchesVat = filter.vat ? customer.vat === filter.vat : true;
-
-    return matchesName && matchesCity && matchesVat;
+    this.filteredData = this.customerService.filterCustomers(filter);
   }
 
   processAdd(customer: Customer){
-    this.customers.push(customer);
-    this.filteredData = this.customers;
+    this.customerService.addCustomer(customer);
+    this.filteredData = this.customerService.getCustomers();
   }
 }
 
