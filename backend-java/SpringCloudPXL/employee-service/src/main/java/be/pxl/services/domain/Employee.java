ef6405e18e94f5602c068ba60e4096a8ab8f1cbe
Su-zenGeurtsPXL <suzen.geurts@student.pxl.be>
2024-10-29 13:05:07 +0100

    [BACKEND] Lab 1: Set up Spring Boot services Set up Root project and Employee Service

---
+++ backend-java/SpringCloudPXL/employee-service/src/main/java/be/pxl/services/domain/Employee.java
@@ -0,0 +1,25 @@
+package be.pxl.services.domain;
+
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Entity
+@Table(name ="employee")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long organizationId;
+    private Long departmentId;
+    private String name;
+    private int age;
+    private String position;
+
+}
